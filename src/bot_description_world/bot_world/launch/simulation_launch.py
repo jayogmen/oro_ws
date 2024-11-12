94db94eae5f6770147ac8e36ac87763f337feb8e
jayogmen <jayaraju@ogmenrobotics.com>
2024-11-12 15:22:44 +0530

    Testing comp updapte

--- src/bot_description_world/bot_world/launch/simulation_launch.py
+++ src/bot_description_world/bot_world/launch/simulation_launch.py
@@ -15,7 +15,7 @@ def generate_launch_description():
         [
             FindPackageShare(package_name),
             "worlds",
-            "example.world"
+            "jayaraju.world"
         ]
     )
    
